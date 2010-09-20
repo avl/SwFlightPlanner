f99c1fdb85bbf9ce3718d006922fe436c72c4639
Anders Musikka <anders.musikka@gmail.com>
2010-09-20 22:48:07 +0200

    various

---
+++ updateheightmap.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#python fplan/lib/create_merc_elevmap.py create /home/anders/saker/avl_fplan_world/elevmap.bin
+python fplan/lib/create_merc_elevmap.py refine /home/anders/saker/avl_fplan_world/elevmap.bin
