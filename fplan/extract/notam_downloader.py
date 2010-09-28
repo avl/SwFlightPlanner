9d54a931f862484e540686e60abf9a75df4cac7b
Anders Musikka <anders.musikka@gmail.com>
2010-09-28 22:05:23 +0200

    notam downloader for finland

--- fplan/extract/notam_downloader.py
+++ fplan/extract/notam_downloader.py
@@ -22,7 +22,15 @@ def download_notam():
     north=urllib2.urlopen("http://www.lfv.se/AISInfo.asp?TextFile=odinesun.txt&SubTitle=&T=SWEDEN%20-%20North&Frequency=250").read()
     mid=urllib2.urlopen(  "http://www.lfv.se/AISInfo.asp?TextFile=odinesos.txt&SubTitle=&T=SWEDEN%20-%20Middle&Frequency=250").read()
     south=urllib2.urlopen("http://www.lfv.se/AISInfo.asp?TextFile=odinesmm.txt&SubTitle=&T=SWEDEN%20-%20South&Frequency=250").read()
-    text="\n".join([north,mid,south,all_notams_but_seldom_updated])
+    
+    finland_n=urllib2.urlopen("http://www.lfv.se/AISInfo.asp?TextFile=odinefps.txt&SubTitle=&T=Finland%20N&Frequency=250").read()
+    finland_s=urllib2.urlopen("http://www.lfv.se/AISInfo.asp?TextFile=odinefes.txt&SubTitle=&T=Finland%20S&Frequency=250").read()
+    finland_all=urllib2.urlopen("http://www.lfv.se/AISInfo.asp?TextFile=odinefin.txt&SubTitle=&T=Finland&Frequency=250").read()
+    
+    text="\n".join([north,mid,south,
+        all_notams_but_seldom_updated,
+        finland_n,finland_s,finland_all
+        ])
     
     outfile="notams/%08d"%(nextnotam,)
     if lasttext!=text:        
