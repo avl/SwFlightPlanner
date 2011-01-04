0406b761c8683d5efdb969afb9f31b78b4e2d82b
Anders Musikka <anders.musikka@gmail.com>
2011-01-04 07:49:08 +0000

    Updated env var

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
-export PYTHONPATH=/home/anders/saker/shapemerge2d:/home/anders/saker/fplan
+export PYTHONPATH=/home/anders/saker/shapemerge2d.git:/home/anders/saker/fplan
 export SWFP_DATADIR=/home/anders/saker/avl_fplan_world
 export SWFP_ROOT=/home/anders/saker/fplan
 
