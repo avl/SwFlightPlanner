860aa01400bfdba796102665d275347011ea74da
Anders Musikka <anders.musikka@gmail.com>
2011-02-05 22:09:17 +0000

    Fix wrong elevation of flightplanner-android gps-tracks

--- fplan/lib/recordings.py
+++ fplan/lib/recordings.py
@@ -72,7 +72,7 @@ def load_recording(rec):
 		maxlon=max(maxlon,lon)
 		minlon=min(minlon,lon)
 		if laststamp==None or stamp-laststamp>cutoff or idx==le-1:
-			out.points.append(((lat,lon),altitude,datetime.utcfromtimestamp(stamp/1000.0)))
+			out.points.append(((lat,lon),altitude*0.3048,datetime.utcfromtimestamp(stamp/1000.0)))
 		laststamp=stamp
 	out.bb1=(maxlat,minlon)
 	out.bb2=(minlat,maxlon)
