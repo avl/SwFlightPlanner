b430227c78f2337b5996b372c6b67053c7800628
Anders Musikka <anders.musikka@gmail.com>
2011-05-03 18:38:08 +0200

    Add missing file

---
+++ fplan/lib/db_env_loader.py
@@ -0,0 +1,21 @@
+from sqlalchemy import engine_from_config
+from paste.deploy import appconfig
+from fplan.config.environment import load_environment
+from fplan.model import meta,User,Trip,Waypoint,Route
+import os
+
+loaded=False
+loading=False
+def ensure_loaded():
+    global loaded
+    global loading
+    if not loaded:
+        assert not loading
+        loading=True  
+        try:
+            conf = appconfig('config:%s'%(os.path.join(os.getcwd(),"development.ini"),))    
+            load_environment(conf.global_conf, conf.local_conf)
+        except:
+            loading=False
+            raise
+        loaded=True  
