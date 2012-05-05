3a1255d8959bfafef5ff9b57a78aa9e24a1bd0f9
anders <anders@ragnar.(none)>
2012-05-05 21:09:02 +0200

    Introduce SWFP_MAPNIK_STYLE environment variable

--- env.sh
+++ env.sh
@@ -4,5 +4,5 @@ export SWFP_DEVCOMP=asus
 export SWFP_DATADIR=/home/anders/saker/avl_fplan_world
 export SWFP_ROOT=/home/anders/saker/fplan
 export SWFP_HOSTNAME=swflightplanner.se
-
+export SWFP_MAPNIK_STYLE=/home/anders/saker/mapnikstyle
 
