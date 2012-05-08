e023cf100da0a5b7c9c24be8343ed2493af0315d
Anders Musikka <anders.musikka@gmail.com>
2012-05-08 08:37:01 +0200

    Updated and expanded list of cities

--- fplan/extract/extract_cities.py
+++ fplan/extract/extract_cities.py
@@ -1,11 +1,22 @@
 
 
+from fplan.lib.maptilereader import latlon_limits,latlon_limits_hd
+        
 
 def get_cities():
     points=[]
+    
     for line in open("fplan/extract/cities.csv"):
         line=unicode(line,'utf8')
-        lon,lat,kind,name=line.strip().split(",")
+        #print repr(line)
+        lon,lat,kind,name=line.strip().split(",",3)
+        name=name.replace('"',"")
+        lon=float(lon)
+        lat=float(lat)
+        lat1,lon1,lat2,lon2=latlon_limits()
+        if kind!='city':
+            if lat<lat1 or lat>lat2 or lon<lon1 or lon>lon2:
+                continue #only take cities from outside the high-def area        
         assert kind in ['city','town']   
         points.append(dict(
             name=name,
@@ -14,5 +25,6 @@ def get_cities():
     return points
 
 if __name__=='__main__':
-    get_cities()
+    cs=get_cities()
+    print "Got %d cities and towns"%(len(cs),)
     
